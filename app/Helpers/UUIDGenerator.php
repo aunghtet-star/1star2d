ab6094bfee87bc500f1ca69f94f9500ca880c512
aunghtet-star <aunghtetthu172@gmail.com>
2022-03-27 14:32:50 +0630

    user account number fix

--- app/Helpers/UUIDGenerator.php
+++ app/Helpers/UUIDGenerator.php
@@ -8,7 +8,7 @@ class UUIDGenerator
 {
     public static function AccountNumber()
     {
-        $number = mt_rand(1000000000000000, 9999999999999999);
+        $number = mt_rand(100000000, 999999999);
         $exist = Wallet::where('account_numbers', $number)->exists();
         if ($exist) {
             self::AccountNumber();
@@ -18,7 +18,7 @@ class UUIDGenerator
 
     public static function RefNumber()
     {
-        $number = mt_rand(1000000000000000, 9999999999999999);
+        $number = mt_rand(100000000, 999999999);
         $exist = Transaction::where('ref_no', $number)->exists();
         if ($exist) {
             self::RefNumber();
@@ -28,7 +28,7 @@ class UUIDGenerator
 
     public static function TrxId()
     {
-        $number = mt_rand(1000000000000000, 9999999999999999);
+        $number = mt_rand(100000000, 999999999);
         $exist = Transaction::where('trx_id', $number)->exists();
         if ($exist) {
             self::TrxId();
